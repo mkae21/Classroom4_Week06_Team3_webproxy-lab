6a90da8af119dbd91cf9605742143af7aad3e447
SUNGJOOLEE <bbuchu123@gmail.com>
2024-02-27 20:57:24 +0900

    echo 완

--- echoserver/echoserveri.c
+++ echoserver/echoserveri.c
@@ -9,18 +9,18 @@ int main(int argc, char **argv)
     struct sockaddr_storage clientaddr; /* Enough space for any address */ // line:netp:echoserveri:sockaddrstorage
     char client_hostname[MAXLINE], client_port[MAXLINE];
 
-    if (argc != 2)
+    if (argc != 2)//잘 못 입력시
     {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
     }
 
-    listenfd = Open_listenfd(argv[1]);
+    listenfd = Open_listenfd(argv[1]);// listen 대기 모드, 클라이언트 반복서버로 하나씩 처리(현재 서버는 한 번에 한 개의 클라이언트만 처리할 수 있다)
     while (1)
     {
         clientlen = sizeof(struct sockaddr_storage);
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE,
+        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, //ip addr -> service string
                     client_port, MAXLINE, 0);
         printf("Connected to (%s, %s)\n", client_hostname, client_port);
         echo(connfd);
